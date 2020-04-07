0bfe9f7c5ee685516b2cde4763ea919c167f476c
NathanLCR <nathan.rocha@maisunifacisa.com.br>
2020-04-07 08:38:16 -0300

    Reformulação do design

---
+++ index.js
@@ -0,0 +1,13 @@
+let loading_line;
+
+window.onload = () => {
+  loading_line = document.getElementById("loading-line");
+};
+
+window.onscroll = (e) => {
+  let d_height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let percentage = (window.scrollY / d_height) * 100;
+  loading_line.style.width = percentage + "%";
+};
